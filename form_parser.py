b6d8e195a030b078c60c6f49c224eb007aecc7e1
Vadim Vlasov <vadim.v.vlasov@gmail.com>
2020-11-21 19:21:18 +0500

    form_parser finished s1-result.csv added

--- form_parser.py
+++ form_parser.py
@@ -60,27 +60,10 @@ def get_html(url):
                     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
                     'Chrome/58.0.3029.110 Safari/537.36',
                 'Accept-Language': 'en-US, en;q=0.5'})
-    r = requests.get(url, headers=headers, verify=False)
+    r = requests.get(url, headers=headers, verify=True)
     if r.ok:
         return r.text
     print(r.status_code)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 def get_report_tables(html, report_name):
